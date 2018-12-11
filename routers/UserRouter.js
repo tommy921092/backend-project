ed97a1a818adf535c4e8714b869665d63b240e66
Oliver Leung <oliver.hon.pan.leung@gmail.com>
2018-12-11 17:19:18 +0800

    implemented changeUsername

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -8,7 +8,8 @@ class UserRouter {
     const router = express.Router();
 
     router.get("/profile", (req, res) => {
-      
+      console.log(req.user.id);
+
       this.UserService.getProfile(req.user.id).then(function(result) {
         console.log('user details:', result)
         res.render(('profile'), { 
@@ -25,8 +26,18 @@ class UserRouter {
     router.post("/changePW", (req, res) => {
       return this.UserService.changePW(req.user.username, req.body.password);
     });
-    router.post("/changeUsername", (req, res) => {
-      return this.UserService.changeusername(req.user.email,req.body.username);
+    
+    router.post("/profile", (req, res) => {
+      this.UserService.changeUsername(req.user.email,req.body.username);
+
+      this.UserService.getProfile(req.user.id).then(function(result) {
+        console.log('user details:', result)
+        res.render(('profile'), { 
+        name: result[0].username,
+        email: result[0].email,
+        profilepic: result[0].profilepicture
+        })
+      });
     });
     return router;
   }
